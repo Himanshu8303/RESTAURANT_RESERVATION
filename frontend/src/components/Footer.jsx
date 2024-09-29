68654ea68ad23463c944b1763038dc75091dfa6a
Himanshu Verma <67681926+Himanshu8303@users.noreply.github.com>
2024-09-29 13:06:17 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">HIMANSHU</div>
+          <div className="left">FOOD ZONE</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Noida, INDIA</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
